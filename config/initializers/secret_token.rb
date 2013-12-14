5d31a91023197287a72ba8dc6e429e768f3d916d
rilian <dmitriyis@gmail.com>
2013-12-14 14:27:01 +0200

    initial rails 4 edge

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blog::Application.config.secret_key_base = '9bdd60b176ed210c5b492fa1091b77c53ac1d9ba97fbab8a8fb5ac8efa0b8159703ede21efbfe4dafee34f6437ccfe1582f5515c7861ea2f9c62f7e17db002bf'
